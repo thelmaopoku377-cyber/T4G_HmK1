efa64918cb2c5ee98709228a8bd006478c667aba
thelmaopoku377-cyber <thelmaopoku377@gmail.com>
2026-07-20 13:17:23 +0000

    added changes

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//prompt the user for their name, age, and email address, and store the values in variables
+let userName = prompt("Enter your name:"); // takes input from the user
+let age = prompt("How old are you"); //takes input from the user
+let email = prompt("Enter your email address:"); // takes input from the user
+
+//alert the user's input
+alert(`Hello ${userName}, you are ${age} years old and your email address is ${email}.`);
