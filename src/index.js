a8ff6d6a867797a25948fd58763e87bd7555bb29
thelmaopoku377-cyber <thelmaopoku377@gmail.com>
2026-07-24 17:16:55 +0000

    Added styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 //prompt the user for their name, age, and email address, and store the values in variables
-let userName = prompt("Enter your name:"); // takes input from the user
+let Name = prompt("Enter your name:"); // takes input from the user
 let age = prompt("How old are you"); //takes input from the user
 let email = prompt("Enter your email address:"); // takes input from the user
 
 //alert the user's input
-alert(`Hello ${userName}, you are ${age} years old and your email address is ${email}.`);
+alert(`Hello ${Name}, you are ${age} years old and your email address is ${email}.`);
 
 // A. Store your name inside a variable and log it
 let name = "Thelma";
@@ -45,3 +45,26 @@ console.log(typeof numVar);
 // J. Set variable to a string and log its type
 let strVar = "Hello World";
 console.log(typeof strVar);
+
+
+//ASSIGMENT10
+
+//User details
+let userName = prompt("Enter your name:");
+let userAge = Number (prompt("Enter your age:"));
+let userEmail = prompt("Enter your email:");
+
+//Check age and display the appropriate message
+if (age < 12){
+   alert(
+    `Hi ${username}, you are ${userAge} years old and you are too young to register.Sorry😔😔`
+   );
+}else if (age >= 12 && age < 18){
+    alert(
+        `Hi ${userName}, you are ${userAge} years old and you have limited options to register for.We will keep in touch via your email: ${email}.👋🏾👋🏾`
+    );
+}else {
+    alert(
+        `Hi ${userName}, you are ${userAge} years old and you can register for any option for your choosing. We will keep in touch via your email: ${email}.👋🏾👋🏾 `
+    );
+}
